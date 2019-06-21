3960e63dc7e7ff10f4906f43e50065f678da7eb1
imooc <271708731@qq.com>
2019-06-21 11:28:03 +0800

    springCloud初始化

---
+++ src/main/java/com/lucheng/springcloud/springcloudexample/SpringcloudexampleApplication.java
@@ -0,0 +1,13 @@
+package com.lucheng.springcloud.springcloudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudexampleApplication.class, args);
+	}
+
+}
