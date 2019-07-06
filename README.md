22928d0bd48a35a1e689958b4953dc11962f0406
imooc <271708731@qq.com>
2019-07-06 12:19:55 +0800

    添加redis缓存系统

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 1.编写springBoot版hello world,编写自测代码
 2.增加天气预报系统子模块
 3.增加根据城市id和城市名称获取天气信息的接口
+4.使用redis缓存系统（单实例）来缓存天气数据，避免频繁调用第三方接口
 
 
 
@@ -18,3 +19,4 @@
 1.了解一下mockMvc的作用
 2.springBoot内置jacson序列化框架。
 可以直接使用objectMapper来将json数据转化为对应的bean对象，需要注意属性名称需要一一对应
+3.redis3.x版本可能链接时会出现保护模式，启动时使用 --protected-mode no参数即可
--- weather_basic/src/main/java/com/weather/basic/weather_basic/ServiceImpl/WeatherDataServiceImpl.java
+++ weather_basic/src/main/java/com/weather/basic/weather_basic/ServiceImpl/WeatherDataServiceImpl.java
@@ -4,18 +4,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weather.basic.weather_basic.EnumUtil.ResponseStatus;
 import com.weather.basic.weather_basic.Response.WeatherResponse;
 import com.weather.basic.weather_basic.Service.WeatherDataService;
+import jdk.nashorn.internal.runtime.regexp.joni.constants.OPSize;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class WeatherDataServiceImpl implements WeatherDataService {
     private static final String WEATHER_DATA_URI = "http://wthrcdn.etouch.cn/weather_mini?";
     @Autowired
     private RestTemplate restTemplate;
+    //redisTemplate对String类型数据的封装
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    public static final Integer REDIS_EXPIRE = 10;
+    //使用log4j来记录日志
+    public static Logger logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
     @Override
     public WeatherResponse getWeatherDataById(String wId) {
         WeatherResponse weatherResponse = new WeatherResponse();
@@ -45,20 +57,30 @@ public class WeatherDataServiceImpl implements WeatherDataService {
     }
 
     private WeatherResponse getWeatherData(String uri,WeatherResponse weatherResponse){
-        //使用httpClint来获取天气信息
-        ResponseEntity<String> responseEntity =  restTemplate.getForEntity(uri,String.class);
-
-        ObjectMapper objectMapper = new ObjectMapper();
+        //是否先查缓存，若缓存不存在通过httpclient调用第三方信息
+        String key = uri;
         String jsonData = null;
-        if(responseEntity.getStatusCodeValue() == 200){
-            jsonData = responseEntity.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        //用来操作key，value数据的数据结构;
+        ValueOperations<String,String> ops = stringRedisTemplate.opsForValue();
+        if(stringRedisTemplate.hasKey(key)){
+            logger.error("redis 中有数据");
+            jsonData = ops.get(key);
+        }else {
+            //使用httpClint来获取天气信息
+            logger.error("redis 中没有数据");
+            ResponseEntity<String> responseEntity =  restTemplate.getForEntity(uri,String.class);
+            if(responseEntity.getStatusCodeValue() == 200){
+                jsonData = responseEntity.getBody();
+            }
+            //将三方数据缓存到redis中,缓存时间为10s
+            ops.set(key,jsonData,REDIS_EXPIRE, TimeUnit.SECONDS);
         }
         try {
             weatherResponse = objectMapper.readValue(jsonData,weatherResponse.getClass());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return weatherResponse;
     }
 
